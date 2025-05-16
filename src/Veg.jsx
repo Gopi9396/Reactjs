e9fc1fbd47c2a652510368fcda70c7cfc9a1b5ae
Gopi9396 <gopiyedilla093@gmail.com>
2025-05-16 16:56:14 +0530

    gopi

--- src/Veg.jsx
+++ src/Veg.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToCart } from './store';
-import './veg.css';
+import './Veg.css';
 
 function Veg() {
   const vegItems = useSelector((state) => state.products.veg);
